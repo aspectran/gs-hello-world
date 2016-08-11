9dfde9b5cacc7bdf7d38569c10296ba23bdd6788
Juho Jeong <aspectran@gmail.com>
2016-08-11 11:12:04 +0900

    Update run.bat

--- app/run.bat
+++ app/run.bat
@@ -1,2 +1,2 @@
 @echo off
-%JAVA_HOME%\bin\java -Dlog4j.configurationFile="file:/%CD%\log4j2.xml" -cp "lib/*" com.aspectran.console.Application
+java -Dlog4j.configurationFile="file:/%CD%\log4j2.xml" -cp "lib/*" com.aspectran.console.Application
